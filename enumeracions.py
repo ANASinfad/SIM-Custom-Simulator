0f76130b19a54fa679adb9a03909ec34474a0bd7
Ponc <ponpcap@hotmail.com>
2021-05-12 23:29:50 +0200

    first commit, added enumerations and created the events class

---
+++ enumeracions.py
@@ -0,0 +1,7 @@
+import enum
+
+class enumeracions(enum.Enum):
+    idle = 0
+    run = 1
+    stop = 2
+    busy = 3
---
+++ event.py
@@ -0,0 +1,7 @@
+from enumeracions import *
+
+class Event:
+    def __init__(self):
+        self.var1 = 0
+        self.var2 = 2
+        self.state = enumeracions.idle
